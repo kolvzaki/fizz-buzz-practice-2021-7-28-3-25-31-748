8a14228146308bf7c09ac513bd6fecb8a434ba44
Kolvzaki <475263961@qq.com>
2022-07-21 13:40:00 +0800

    test:should_return_fizz_when_countOff_given_3

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -1,4 +1,10 @@
 package tdd.fizzbuzz;
 
 public class FizzBuzz {
+    public String countOff(int number){
+        if (number%3==0){
+            return "Fizz";
+        }
+        return String.valueOf(number);
+    }
 }
--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -1,4 +1,17 @@
 package tdd.fizzbuzz;
 
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class FizzBuzzTest {
+
+    @Test
+    public void should_return_fizz_when_countOff_given_3(){
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        int number = 3;
+        String expected = "Fizz";
+        String output = fizzBuzz.countOff(number);
+
+        assertThat(output).isEqualTo(expected);
+    }
 }
